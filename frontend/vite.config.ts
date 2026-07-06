ccb53de73bfd8981831ecf08bddf3eaf0fcd3e00
xiaofenBGI <289390222@qq.com>
2026-07-06 19:16:44 +0800

    modify vite.config.ts for local build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,4 @@
 // vite.config.ts
-//import { defineConfig } from 'vite'
-//import vue from '@vitejs/plugin-vue'
-//import { resolve } from 'path'
-
-//export default defineConfig({
-//  plugins: [vue()],
-//  resolve: {
- //   alias: {
-//   '@': resolve(__dirname, 'src')
-//    }
-//  },
-//  server: {
-//   port: 3000,
-//    open: true
-//  }
-//})
-
-// vite.config.ts
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
@@ -25,13 +7,31 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+   '@': resolve(__dirname, 'src')
     }
   },
   server: {
-    port: 3000,
-    open: true
-  },
-  // 新增：设置基础路径为仓库名，适配 GitHub Pages 子目录部署
-  base: '/enterprise-org-governance/'
+  port: 3000,
+   open: true
+   }
 })
+
+// vite.config.ts
+//import { defineConfig } from 'vite'
+//import vue from '@vitejs/plugin-vue'
+//import { resolve } from 'path'
+
+//export default defineConfig({
+//  plugins: [vue()],
+//  resolve: {
+//    alias: {
+//      '@': resolve(__dirname, 'src')
+//    }
+ // },
+ // server: {
+  //  port: 3000,
+ //   open: true
+  //},
+  // 新增：设置基础路径为仓库名，适配 GitHub Pages 子目录部署
+ // base: '/enterprise-org-governance/'
+//})
